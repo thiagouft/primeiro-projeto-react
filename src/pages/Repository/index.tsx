3b4362085326ea857c92b10c8b8267faf9f7c355
Thiago Silva Pereira <thiagoufma@gmail.com>
2021-05-17 23:01:07 -0300

    Estilizando detalhe

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+import logoImg from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
@@ -7,7 +11,54 @@ interface RepositoryParams {
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
-  return <h1>Repository:{params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="GitHub Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars.githubusercontent.com/u/69631?v=4"
+            alt="Facebook"
+          />
+          <div>
+            <strong>facebook/react</strong>
+            <p>Descrição do Repo</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>1808</strong>
+            <span>Issues Abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="asdasd">
+          <div>
+            <strong>zxczx</strong>
+            <p>zxczxc</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
